40eddaa3ac9cb77758c34c3e81ad3356c38253e1
C9Lloyd <jacielramirez2000@gmail.com>
2024-01-29 08:40:30 -0600

    xd
    xd

--- EnvíosJADEE/Network/ModulosService.cs
+++ EnvíosJADEE/Network/ModulosService.cs
@@ -68,5 +68,29 @@ namespace EnvíosJADEE.Network
 
         }
 
+        public void UpdateModulos(ModuloModel modulo)
+        {
+            parametros = new ArrayList();
+            parametros.Add(new SqlParameter { ParameterName = "@pId", SqlDbType = System.Data.SqlDbType.Int, Value = modulo.Id });
+            parametros.Add(new SqlParameter { ParameterName = "@pNombre", SqlDbType = System.Data.SqlDbType.VarChar, Value = modulo.Nombre });
+            parametros.Add(new SqlParameter { ParameterName = "@pIdCategoria", SqlDbType = System.Data.SqlDbType.Int, Value = modulo.IdCategoria });
+            parametros.Add(new SqlParameter { ParameterName = "@pUsuario", SqlDbType = System.Data.SqlDbType.Int, Value = SesionClass.IdUsuario });
+            parametros.Add(new SqlParameter { ParameterName = "@pCategoria", SqlDbType = System.Data.SqlDbType.VarChar, Value = modulo.Categoria });
+            parametros.Add(new SqlParameter { ParameterName = "@pEstatus", SqlDbType = System.Data.SqlDbType.Int, Value = modulo.Estatus == "Activo" ? 1 : 0 });
+            parametros.Add(new SqlParameter { ParameterName = "@pUsuario", SqlDbType = System.Data.SqlDbType.Int, Value = SesionClass.IdUsuario });
+
+
+            try
+            {
+                dac.ExecuteNonQuery("UpdateModulos", parametros);
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return;
+        }
+
     }
 }
